46669afc89d66914ff6cd41227b64c9394d6e624
pratiksurti <pratik.surti17@gmail.com>
2021-08-28 10:43:22 -0400

    Initial commit

---
+++ src/main/java/com/ps/wikicoordinator/WikiCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.ps.wikicoordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiCoordinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/wikicoordinator/WikiCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.wikicoordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiCoordinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
